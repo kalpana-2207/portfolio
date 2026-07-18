ce3d386ad56015bd5abc435c82d5f594fb381b0c
kalpana-2207 <kalpanathangamuthu7@gmail.com>
2026-07-18 11:44:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 
+const roles = [
+  "Embedded Systems Engineer",
+  "IoT Developer",
+  "PCB Designer",
+  "Electronics Enthusiast"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+  if (!typing) return;
+
+  const current = roles[roleIndex];
+
+  if (!deleting) {
+    typing.textContent = current.substring(0, charIndex++);
+  } else {
+    typing.textContent = current.substring(0, charIndex--);
+  }
+
+  let speed = deleting ? 60 : 120;
+
+  if (!deleting && charIndex === current.length + 1) {
+    deleting = true;
+    speed = 1500;
+  }
+
+  if (deleting && charIndex === 0) {
+    deleting = false;
+    roleIndex = (roleIndex + 1) % roles.length;
+  }
+
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
